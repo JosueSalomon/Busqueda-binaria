ba5f74fc6b1b7e8a9d302e6d384c0a973595660d
JosueSalomon <josueisacsalomonlanda@gmail.com>
2023-09-26 23:43:51 -0600

    BusquedaBinaria en Java

---
+++ BusquedaBinaria.java
@@ -0,0 +1,41 @@
+public class BusquedaBinaria {
+    public static void main(String[] args) {
+
+        //El arreglo tiene que estar ordenado 
+        int[] miArreglo = {1,2,3,4,5,6};
+        int[] miArreglo2 = {22,27,30,35,45};
+
+        BusquedaBinaria(miArreglo2,22);
+        BusquedaBinaria(miArreglo,7);
+    }
+
+    public static void BusquedaBinaria(int[] arregloOrdenado, int numeroBuscar){
+        int menorIndice;
+        int mayorIndice;
+        int mitadIndiceArreglo=0;
+        boolean centinela = false;
+        menorIndice=0;
+        mayorIndice=arregloOrdenado.length-1;
+        while(menorIndice <= mayorIndice){
+            mitadIndiceArreglo= (mayorIndice+menorIndice)/2;
+
+            if(arregloOrdenado[mitadIndiceArreglo]==numeroBuscar){
+                System.out.println("Se encontro el numero ");
+                centinela = true;
+                break;
+            }
+            if(arregloOrdenado[mitadIndiceArreglo] > numeroBuscar){
+                mayorIndice = mitadIndiceArreglo - 1;
+            }
+            if(arregloOrdenado[mitadIndiceArreglo] < numeroBuscar){
+                menorIndice = mitadIndiceArreglo + 1;
+            }
+        }
+
+        if(centinela==true){
+            System.out.println("El numero se encontro en la posicion "+mitadIndiceArreglo);
+        }else{
+            System.out.println("No encontro el numero");
+        }
+    }
+}
